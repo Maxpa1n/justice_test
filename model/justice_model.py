fbdbd34d409b6df2aa4adfdf7952b520d27a2529
song_ran <775126459@qq.com>
2020-06-18 23:38:46 +0800

    add bert model

--- model/justice_model.py
+++ model/justice_model.py
@@ -3,7 +3,7 @@ from torch.nn import CrossEntropyLoss, MSELoss, BCEWithLogitsLoss, BCELoss
 
 from model.file_utils import add_start_docstrings_to_callable
 from model.modeling_albert import AlbertPreTrainedModel, AlbertModel, ALBERT_INPUTS_DOCSTRING
-from model.modeling_bert import BertModel
+from model.modeling_bert import BertModel, BertPreTrainedModel
 import torch.nn as nn
 
 from model.modeling_bert import BERT_INPUTS_DOCSTRING
@@ -197,3 +197,97 @@ class AlbertForJustice(AlbertPreTrainedModel):
             outputs = (loss,) + outputs
 
         return outputs  # (loss), reshaped_logits, (hidden_states), (attentions)
+
+    class BertForJustice(BertPreTrainedModel):
+        def __init__(self, config):
+            super().__init__(config)
+
+            self.bert = BertModel(config)
+            self.dropout = nn.Dropout(config.hidden_dropout_prob)
+            self.classifier = nn.Linear(config.hidden_size * 4, 16)
+
+            self.init_weights()
+
+        @add_start_docstrings_to_callable(ALBERT_INPUTS_DOCSTRING)
+        def forward(
+                self,
+                input_ids=None,
+                attention_mask=None,
+                token_type_ids=None,
+                position_ids=None,
+                head_mask=None,
+                inputs_embeds=None,
+                labels=None,
+        ):
+            r"""
+            labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`, defaults to :obj:`None`):
+                Labels for computing the multiple choice classification loss.
+                Indices should be in ``[0, ..., num_choices]`` where `num_choices` is the size of the second dimension
+                of the input tensors. (see `input_ids` above)
+
+        Returns:
+            :obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.BertConfig`) and inputs:
+            loss (:obj:`torch.FloatTensor` of shape `(1,)`, `optional`, returned when :obj:`labels` is provided):
+                Classification loss.
+            classification_scores (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, num_choices)`):
+                `num_choices` is the second dimension of the input tensors. (see `input_ids` above).
+
+                Classification scores (before SoftMax).
+            hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_hidden_states=True``):
+                Tuple of :obj:`torch.FloatTensor` (one for the eval_model_dir of the embeddings + one for the eval_model_dir of each layer)
+                of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+
+                Hidden-states of the model at the eval_model_dir of each layer plus the initial embedding outputs.
+            attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``config.output_attentions=True``):
+                Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+                :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+
+                Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+                heads.
+
+        Examples::
+
+            from transformers import BertTokenizer, BertForMultipleChoice
+            import torch
+
+            tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+            model = BertForMultipleChoice.from_pretrained('bert-base-uncased')
+            choices = ["Hello, my dog is cute", "Hello, my cat is amazing"]
+
+            input_ids = torch.tensor([tokenizer.encode(s, add_special_tokens=True) for s in choices]).unsqueeze(0)  # Batch size 1, 2 choices
+            labels = torch.tensor(1).unsqueeze(0)  # Batch size 1
+            outputs = model(input_ids, labels=labels)
+
+            loss, classification_scores = outputs[:2]
+
+            """
+            num_choices = input_ids.shape[1]
+            batch_size = input_ids.shape[0]
+
+            input_ids = input_ids.view(-1, input_ids.size(-1))
+            attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+            token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+            position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+
+            outputs = self.bert(
+                input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+            )
+
+            pooled_output = outputs[1]
+
+            pooled_output = self.dropout(pooled_output)
+            reshaped_logits = pooled_output.view(batch_size, -1)
+            logits = self.classifier(reshaped_logits)
+            outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+
+            if labels is not None:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits, labels)
+                outputs = (loss,) + outputs
+
+            return outputs  # (loss), reshaped_logits, (hidden_states), (attentions)
